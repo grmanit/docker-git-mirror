ccf800db613f4915e86a042c32c77530806fb1cb
Peter Grman <peter.grman@gmail.com>
2016-05-21 19:25:23 +0200

    wait for git to finish

--- scripts/init_mirrors.py
+++ scripts/init_mirrors.py
@@ -9,5 +9,5 @@ print "Executing python script in " + base_dir
 for line in open(sys.argv[2]):
     if line and not line.isspace() and line[0] != '#': # last check is a simple way to comment out lines
         [name, source, destination] = line.strip().split("|")
-        subprocess.Popen(["git", "clone", "--mirror", source, name], cwd=base_dir)
-        subprocess.Popen(["git", "remote", "set-url", "--push", "origin", destination], cwd=path.join(base_dir, name))
+        subprocess.Popen(["git", "clone", "--mirror", source, name], cwd=base_dir).wait()
+        subprocess.Popen(["git", "remote", "set-url", "--push", "origin", destination], cwd=path.join(base_dir, name)).wait()
