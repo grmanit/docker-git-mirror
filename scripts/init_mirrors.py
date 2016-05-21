fbe0ffcc46125ab4c56fae46388a2737a10c095f
Peter Grman <peter.grman@gmail.com>
2016-05-21 17:35:47 +0200

    allow empty lines and comments

--- scripts/init_mirrors.py
+++ scripts/init_mirrors.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 
 for line in open(sys.argv[1]):
-    [name, source, destination] = line.strip().split("|")
-    subprocess.Popen(["git", "clone", "--mirror", source, name])
-    subprocess.Popen(["git", "remote", "set-url", "--push", "origin", destination])
+    if line and not line.isspace() and line[0] != '#': # last check is a simple way to comment out lines
+        [name, source, destination] = line.strip().split("|")
+        subprocess.Popen(["git", "clone", "--mirror", source, name])
+        subprocess.Popen(["git", "remote", "set-url", "--push", "origin", destination])
