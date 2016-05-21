4d9d178c2c9ab686377edfb92acb8294c764c909
Peter Grman <peter.grman@gmail.com>
2016-05-21 17:59:18 +0200

    add debug info and fix subprocess.Popen call

--- scripts/init_mirrors.py
+++ scripts/init_mirrors.py
@@ -1,8 +1,11 @@
 import subprocess
 import sys
+import os
+
+print "Executing python script in " + os.getcwd()
 
 for line in open(sys.argv[1]):
     if line and not line.isspace() and line[0] != '#': # last check is a simple way to comment out lines
         [name, source, destination] = line.strip().split("|")
         subprocess.Popen(["git", "clone", "--mirror", source, name])
-        subprocess.Popen(["git", "remote", "set-url", "--push", "origin", destination])
+        subprocess.Popen(["git", "remote", "set-url", "--push", "origin", destination], cwd=name)
